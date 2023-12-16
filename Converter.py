fbc370ec54586130b3fad0539879b714aebb2936
WarPigC <85150042+WarPigC@users.noreply.github.com>
2023-12-16 21:24:33 +0530

    ...

--- Converter.py
+++ Converter.py
@@ -1,7 +1,7 @@
-import random as r
+from random import randint
 from subprocess import run
 
 def convert(video):
-    name = str(r.randint(1,1000))
+    name = str(randint(1,1000))
     run(f'''ffmpeg -i {str(video)} -filter_complex "[0:v] fps=15,scale=w=-1:h=300,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y {name}.gif''')
     return name   
